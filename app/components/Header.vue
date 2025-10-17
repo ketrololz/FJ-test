94e38fb2931f669b390ed906f1ec2daf49a6d310
ketrololz <bqnexxx@gmail.com>
2025-10-17 22:16:04 +0300

    feat: add detailed page  load skeleton

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+interface Props {
+  search?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  search: true,
+});
+
 const emit = defineEmits<{
   (e: "change", value: string): void;
 }>();
@@ -11,12 +19,13 @@ watch(input, () => {
 </script>
 <template>
   <div
-    class="border-b border-neutral-300 py-2 flex gap-10 justify-between md:justify-start"
+    class="border-b border-neutral-300 py-2 h-14 flex items-center gap-10 justify-between md:justify-start"
   >
     <NuxtLink to="/">
       <span class="text-xl font-bold">CarCatalog</span>
     </NuxtLink>
     <UInput
+      v-if="search"
       icon="i-lucide-search"
       color="neutral"
       variant="soft"
--- app/pages/cars/[id].vue
+++ app/pages/cars/[id].vue
@@ -4,21 +4,26 @@ import type { Car } from "~/types/Car";
 const router = useRouter();
 const route = useRoute();
 const carsStore = useCarsStore();
+const { cars } = storeToRefs(carsStore);
 
-const data = ref();
-
+const isLoading = ref(false);
 const car = ref<Car>();
-
 const selectedCar = ref(null);
 
 async function loadDetails(id: string) {
-  const { data } = await useFetch(`/api/cars/${id}`);
-  selectedCar.value = data.value[0];
+  try {
+    isLoading.value = true;
+    const data = await $fetch(`/api/cars/${id}`);
+    selectedCar.value = data[0];
+  } catch (e) {
+    console.error("Не удалось загрузить список автомобилей", e);
+  } finally {
+    isLoading.value = false;
+  }
 }
 
 onMounted(async () => {
   await carsStore.loadCars();
-  const { cars, isLoading } = storeToRefs(carsStore);
 
   const value = cars.value.find((car) => car.modelId === route.params.id);
 
@@ -31,63 +36,74 @@ onMounted(async () => {
   await loadDetails(car.value!.modelId);
 });
 </script>
+
 <template>
   <div class="flex flex-col gap-10">
-    <Header></Header>
-    <div v-if="car && selectedCar" class="flex flex-col gap-4 md:flex-row">
-      <div
-        class="p-4 w-full h-full flex flex-col bg-neutral-100 border border-neutral-300/70 rounded-xl overflow-hidden min-h-50 shadow-sm shadow-neutral-200/35"
-      >
-        <h3 class="text-lg font-bold text-default mb-4">
-          {{ car.make }}
-        </h3>
-        <div class="flex flex-col">
-          <p class="text-neutral-400">
-            Модель:
-            <span class="text-sm text-default font-semibold">
-              {{ car.model }}
-            </span>
-          </p>
-          <p class="text-neutral-400">
-            Год выпуска:
-            <span class="text-sm text-default font-semibold">
-              {{ car.year }}
-            </span>
-          </p>
-          <p class="text-neutral-400" v-if="selectedCar['make_country']">
-            Страна:
-            <span class="text-sm text-default font-semibold">
-              {{ selectedCar["make_country"] }}
-            </span>
-          </p>
-          <p class="text-neutral-400" v-if="selectedCar['model_engine_l']">
-            Объём двигателя:
-            <span class="text-sm text-default font-semibold">
-              {{ selectedCar["model_engine_l"] }} л
-            </span>
-          </p>
-          <p
-            class="text-neutral-400"
-            v-if="selectedCar['model_engine_power_hp']"
-          >
-            Мощность двигателя:
-            <span class="text-sm text-default font-semibold">
-              {{ selectedCar["model_engine_power_hp"] }} л/c
-            </span>
-          </p>
-          <p class="text-neutral-400" v-if="selectedCar['model_doors']">
-            Кол-во дверей:
-            <span class="text-sm text-default font-semibold">
-              {{ selectedCar["model_doors"] }}
-            </span>
-          </p>
+    <Header :search="false"></Header>
+    <div class="flex flex-col gap-4 md:flex-row">
+      <template v-if="isLoading">
+        <USkeleton
+          class="h-50 w-full rounded-xl bg-neutral-200 animate-pulse"
+        />
+        <USkeleton
+          class="h-80 w-full rounded-xl bg-neutral-200 animate-pulse"
+        />
+      </template>
+      <template v-if="car && selectedCar">
+        <div
+          class="p-4 w-full h-full flex flex-col bg-neutral-100 border border-neutral-300/70 rounded-xl overflow-hidden min-h-50 shadow-sm shadow-neutral-200/35"
+        >
+          <h3 class="text-lg font-bold text-default mb-4">
+            {{ car.make }}
+          </h3>
+          <div class="flex flex-col">
+            <p class="text-neutral-400">
+              Модель:
+              <span class="text-sm text-default font-semibold">
+                {{ car.model }}
+              </span>
+            </p>
+            <p class="text-neutral-400">
+              Год выпуска:
+              <span class="text-sm text-default font-semibold">
+                {{ car.year }}
+              </span>
+            </p>
+            <p class="text-neutral-400" v-if="selectedCar['make_country']">
+              Страна:
+              <span class="text-sm text-default font-semibold">
+                {{ selectedCar["make_country"] }}
+              </span>
+            </p>
+            <p class="text-neutral-400" v-if="selectedCar['model_engine_l']">
+              Объём двигателя:
+              <span class="text-sm text-default font-semibold">
+                {{ selectedCar["model_engine_l"] }} л
+              </span>
+            </p>
+            <p
+              class="text-neutral-400"
+              v-if="selectedCar['model_engine_power_hp']"
+            >
+              Мощность двигателя:
+              <span class="text-sm text-default font-semibold">
+                {{ selectedCar["model_engine_power_hp"] }} л/c
+              </span>
+            </p>
+            <p class="text-neutral-400" v-if="selectedCar['model_doors']">
+              Кол-во дверей:
+              <span class="text-sm text-default font-semibold">
+                {{ selectedCar["model_doors"] }}
+              </span>
+            </p>
+          </div>
         </div>
-      </div>
-      <NuxtImg
-        v-if="car.imageUrl"
-        :src="car.imageUrl"
-        class="h-full w-full md:w-[50%] max-h-100 object-cover object-center rounded-xl"
-      ></NuxtImg>
+        <NuxtImg
+          v-if="car.imageUrl"
+          :src="car.imageUrl"
+          class="h-full w-full md:w-[50%] max-h-100 object-cover object-center rounded-xl"
+        ></NuxtImg>
+      </template>
     </div>
   </div>
 </template>
